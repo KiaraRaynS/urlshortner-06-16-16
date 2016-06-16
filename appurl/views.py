e4089948fe3d09976b4f2bcb5fcadce607a29e0b
KiaraRaynS <sherman.rayn@outlook.com>
2016-06-16 14:49:46 -0400

    Project initialized

---
+++ appurl/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views.generic import View, CreateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+class ViewIndex(View):
+
+    def get(self, request):
+        return HttpResponse("Hello, world!")
+
+
+class SignUpView(CreateView):
+    model = User
+    form_class = UserCreationForm
